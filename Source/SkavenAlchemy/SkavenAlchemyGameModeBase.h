2269b97c2452c371fda09c4c32f136464069cd34
JamesAncira <30585938+JamesAncira@users.noreply.github.com>
2019-10-17 21:08:32 -0500

    Initial commit

---
+++ Source/SkavenAlchemy/SkavenAlchemyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkavenAlchemyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKAVENALCHEMY_API ASkavenAlchemyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
